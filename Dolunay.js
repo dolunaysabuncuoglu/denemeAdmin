cb8299a3cf0e93fcba20fb37aa0caee9d87a0cb5
dolunaysabuncuoglu <72995499+dolunaysabuncuoglu@users.noreply.github.com>
2022-01-31 21:02:15 +0300

    Update Dolunay.js

--- Dolunay.js
+++ Dolunay.js
@@ -6,10 +6,7 @@
     return fetch("https://www.fintag.org/callback?code=47369119", {
       method: "GET",
       mode: 'no-cors', // It can be no-cors, cors, same-origin
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
+      
     }).then((response) => response.json())
     .then((response) => {
       console.log(response);
